362870a3d5770bda44a4d435b07b320bb273132f
vsvami <106614918+vsvami@users.noreply.github.com>
2024-03-05 16:32:36 +0300

    setup project

---
+++ MoneyAccounting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoneyAccounting
+//
+//  Created by Vladimir Dmitriev on 05.03.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
