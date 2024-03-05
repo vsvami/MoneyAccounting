f1cd14dbaff3e1e4427bac40afe2153da0b29045
Ivan Semikin <i.semikin.94@icloud.com>
2024-03-05 22:02:36 +0100

    Created directories for all ViewControllers.

---
+++ MoneyAccounting/Scenes/Person/PersonViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PersonViewController.swift
+//  MoneyAccounting
+//
+//  Created by Иван Семикин on 05/03/2024.
+//
+
+import UIKit
+
+class PersonViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
