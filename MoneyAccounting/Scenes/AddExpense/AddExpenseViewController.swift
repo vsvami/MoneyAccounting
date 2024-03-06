968fa07b36d7ffe373d65bec3abc34da1569d05d
Ivan Semikin <i.semikin.94@icloud.com>
2024-03-06 11:07:39 +0100

    Added two directories: AddIncome, AddExpense

---
+++ MoneyAccounting/Scenes/AddExpense/AddExpenseViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddExpenseViewController.swift
+//  MoneyAccounting
+//
+//  Created by Иван Семикин on 06/03/2024.
+//
+
+import UIKit
+
+class AddExpenseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
