19a2a5025f81ca989a148a60c34e1bd14fdfff62
vsvami <106614918+vsvami@users.noreply.github.com>
2024-03-07 01:37:28 +0300

    setup main storyboard

--- MoneyAccounting/Scenes/Main/MainViewController.swift
+++ MoneyAccounting/Scenes/Main/MainViewController.swift
@@ -9,6 +9,17 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    @IBOutlet var balanceLabel: UILabel!
+    @IBOutlet var incomeLabel: UILabel!
+    @IBOutlet var targetIncomeLabel: UILabel!
+    @IBOutlet var expenseLabel: UILabel!
+    @IBOutlet var targetExpenseLabel: UILabel!
+    
+    @IBOutlet var addIncomeButton: UIButton!
+    @IBOutlet var addExpenseButton: UIButton!
+    @IBOutlet var showIncomeButton: UIButton!
+    @IBOutlet var showExpenseButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
