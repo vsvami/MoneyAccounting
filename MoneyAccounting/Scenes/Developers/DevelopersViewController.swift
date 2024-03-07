1b5ddadd31acf76befc7a2cee4ba020f47737a19
Tatiana Lazarenko <tan.strebkova@gmail.com>
2024-03-07 12:36:33 +0300

    set UI for developers screen

--- MoneyAccounting/Scenes/Developers/DevelopersViewController.swift
+++ MoneyAccounting/Scenes/Developers/DevelopersViewController.swift
@@ -9,21 +9,36 @@ import UIKit
 
 class DevelopersViewController: UIViewController {
 
+    @IBOutlet var alexeyImageView: UIImageView!
+    @IBOutlet var nikitaImageView: UIImageView!
+    @IBOutlet var dariaImageView: UIImageView!
+    @IBOutlet var tatianaImageView: UIImageView!
+    @IBOutlet var vladimirImageView: UIImageView!
+    @IBOutlet var ivanImageView: UIImageView!
+    
+    @IBOutlet var donationButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        alexeyImageView.image = UIImage(named: "alexey")
+        nikitaImageView.image = UIImage(named: "nikita")
+        tatianaImageView.image = UIImage(named: "tatiana")
+        vladimirImageView.image = UIImage(named: "vladimir")
+        dariaImageView.image = UIImage(named: "tc")
+        ivanImageView.image = UIImage(named: "tc")
+        
+        alexeyImageView.layer.cornerRadius = alexeyImageView.frame.height / 2
+        nikitaImageView.layer.cornerRadius = nikitaImageView.frame.height / 2
+        tatianaImageView.layer.cornerRadius = tatianaImageView.frame.height / 2
+        vladimirImageView.layer.cornerRadius = vladimirImageView.frame.height / 2
+        
+        donationButton.backgroundColor = UIColor(
+            red: 249/255,
+            green: 249/255,
+            blue: 252/255,
+            alpha: 1
+        )
+        donationButton.layer.cornerRadius = donationButton.frame.height / 2
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
