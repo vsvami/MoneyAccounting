dbc5d6c1d196c3b8a826eac678f9a4da230b39e1
vsvami <106614918+vsvami@users.noreply.github.com>
2024-03-07 03:59:27 +0300

    add uiview+ext

--- MoneyAccounting/Extensions/UIView+Ext.swift
+++ MoneyAccounting/Extensions/UIView+Ext.swift
@@ -5,4 +5,13 @@
 //  Created by Vladimir Dmitriev on 07.03.24.
 //
 
-import Foundation
+import UIKit
+
+extension UIView {
+   func roundCorners(corners: UIRectCorner, radius: CGFloat) {
+        let path = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        let mask = CAShapeLayer()
+        mask.path = path.cgPath
+        layer.mask = mask
+    }
+}
