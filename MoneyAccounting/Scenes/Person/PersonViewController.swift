dd7d77e1525cb7b4266ebb6bba42c4a7e65acd3c
KDaria <dkdva@mail.ru>
2024-03-07 20:58:33 +0100

    Set up user interface for PersonViewController - Added rounded corners to photoImageView and grayView. - Configured logOutButton with system blue background and rounded corners. - Implemented addPhotoAction to allow photo picking from camera or library. - Established UIImagePickerControllerDelegate to handle image picking process. - Added privacy for photo.

--- MoneyAccounting/Scenes/Person/PersonViewController.swift
+++ MoneyAccounting/Scenes/Person/PersonViewController.swift
@@ -2,28 +2,77 @@
 //  PersonViewController.swift
 //  MoneyAccounting
 //
-//  Created by Иван Семикин on 05/03/2024.
-//
 
 import UIKit
 
-class PersonViewController: UIViewController {
-
+final class PersonViewController: UIViewController {
+    //MARK: - IB Outlets
+    @IBOutlet var photoImageView: UIImageView!
+    
+    @IBOutlet var logOutButton: UIButton!
+    
+    @IBOutlet var grayView: UIView!
+    
+    @IBOutlet var firstNameLabel: UILabel!
+    @IBOutlet var lastNameLabel: UILabel!
+    @IBOutlet var passwordLabel: UILabel!
+    @IBOutlet var emailLabel: UILabel!
+    
+    //MARK: Life Circle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        photoImageView.layer.cornerRadius = photoImageView.frame.height / 2
+        photoImageView.image = UIImage.tc
+        
+        grayView.layer.cornerRadius = 15
+        
+        logOutButton.backgroundColor = .systemBlue
+        logOutButton.tintColor = .white
+        logOutButton.layer.cornerRadius = logOutButton.frame.height / 2
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func addPhotoAction() {
+        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        let camera = UIAlertAction(title: "Camera", style: .default) { _ in
+            self.chooseImagePicker(source: .camera)
+        }
+        
+        let photo = UIAlertAction(title: "Photo", style: .default) { _ in
+            self.chooseImagePicker(source: .photoLibrary)
+        }
+        
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        actionSheet.addAction(camera)
+        actionSheet.addAction(photo)
+        actionSheet.addAction(cancel)
+        
+        present(actionSheet, animated: true)
     }
-    */
-
+    
 }
+
+//MARK: Work with image
+extension PersonViewController: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    func chooseImagePicker(source: UIImagePickerController.SourceType) {
+        if UIImagePickerController.isSourceTypeAvailable(source) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.allowsEditing = true
+            imagePicker.sourceType = source
+            present(imagePicker, animated: true)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        photoImageView.image = info[.editedImage] as? UIImage
+        photoImageView.contentMode = .scaleAspectFit
+        photoImageView.clipsToBounds = true
+        
+        dismiss(animated: true)
+    }
+}
+
