351d2329715d57858e3c7481d5941a51dc62d439
Ivan Semikin <i.semikin.94@icloud.com>
2024-03-08 19:44:40 +0100

    Update CategoryVC with setup back button.

--- MoneyAccounting/Scenes/Category/CategoryViewController.swift
+++ MoneyAccounting/Scenes/Category/CategoryViewController.swift
@@ -13,5 +13,25 @@ class CategoryViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupBackBarButtonItem()
+    }
+    
+    @objc private func backButtonPressed() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    private func setupBackBarButtonItem() {
+        let arrowImage = UIImage(systemName: "arrow.backward")
+        
+        let backButton = UIButton(type: .system)
+        backButton.setImage(arrowImage, for: .normal)
+        backButton.setTitle("Назад", for: .normal)
+        backButton.sizeToFit()
+        backButton.addTarget(self, action: #selector(backButtonPressed), for: .touchUpInside)
+        backButton.tintColor = .systemBlue
+        
+        let backBarButtonItem = UIBarButtonItem(customView: backButton)
+        
+        navigationItem.leftBarButtonItem = backBarButtonItem
     }
 }
