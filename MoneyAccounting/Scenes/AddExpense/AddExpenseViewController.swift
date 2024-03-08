3cbc4f00dc1747a94fb02a4b096b319a06882f16
Tatiana Lazarenko <tan.strebkova@gmail.com>
2024-03-08 15:14:03 +0300

    change textFields to Buttons, set text color

--- MoneyAccounting/Scenes/AddExpense/AddExpenseViewController.swift
+++ MoneyAccounting/Scenes/AddExpense/AddExpenseViewController.swift
@@ -9,18 +9,20 @@ import UIKit
 
 final class AddExpenseViewController: UIViewController {
 
-    @IBOutlet var dateTextField: UITextField!
     @IBOutlet var nameExpenceTextField: UITextField!
-    @IBOutlet var chooseCategoryTextField: UITextField!
     @IBOutlet var addAmountTextField: UITextField!
+    
+    @IBOutlet var dateButton: UIButton!
+    @IBOutlet var chooseCategoryButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setTextField(dateTextField)
-        setTextField(nameExpenceTextField)
-        setTextField(chooseCategoryTextField)
-        setTextField(addAmountTextField)
+        setTextField(nameExpenceTextField, withText: "Название расхода")
+        setTextField(addAmountTextField, withText: "Введите сумму")
+        
+        setButton(dateButton)
+        setButton(chooseCategoryButton)
     }
     
     @IBAction func addAction() {
@@ -31,7 +33,7 @@ final class AddExpenseViewController: UIViewController {
         dismiss(animated: true)
     }
     
-    private func setTextField(_ textField: UITextField) {
+    private func setTextField(_ textField: UITextField, withText placeholder: String) {
         textField.setLeftPaddingPoints(20)
         textField.setRightPaddingPoints(20)
         textField.setBorderStyle()
@@ -41,6 +43,34 @@ final class AddExpenseViewController: UIViewController {
             green: 249/255,
             blue: 252/255,
             alpha: 1
+        )
+        textField.attributedPlaceholder = NSAttributedString(
+            string: placeholder,
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor(
+                red: 134/255,
+                green: 143/255,
+                blue: 165/255,
+                alpha: 1
+            )]
+        )
+    }
+    
+    private func setButton(_ button: UIButton) {
+        button.backgroundColor = UIColor(
+            red: 249/255,
+            green: 249/255,
+            blue: 252/255,
+            alpha: 1
+        )
+        button.layer.cornerRadius = button.frame.height / 2
+        button.setTitleColor(
+            UIColor(
+                red: 134/255,
+                green: 143/255,
+                blue: 165/255,
+                alpha: 1
+            ),
+            for: .normal
         )
     }
 }
