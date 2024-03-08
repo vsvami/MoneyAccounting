cd2dea4d5fe832f9fefed67d63a719d47fddeb45
KDaria <dkdva@mail.ru>
2024-03-08 22:27:22 +0100

    Added new class TransactionStore to DataStore

--- MoneyAccounting/Services/DataStore.swift
+++ MoneyAccounting/Services/DataStore.swift
@@ -14,46 +14,77 @@ final class DataStore {
         Category(
             name: "Здоровье",
             icon: "cross.case.fill",
-            colorImage: UIImage(named: "gradient1") ?? .backgroundMain),
+            colorImage: "gradient1"),
         Category(
             name: "Одежда",
             icon: "tshirt.fill",
-            colorImage: UIImage(named: "gradient2") ?? .backgroundMain),
+            colorImage: "gradient2"),
         Category(
             name: "Питомцы",
             icon: "pawprint.fill",
-            colorImage: UIImage(named: "gradient3") ?? .backgroundMain),
+            colorImage: "gradient3"),
         Category(
             name: "Продукты",
             icon: "cart.fill",
-            colorImage: UIImage(named: "gradient4") ?? .backgroundMain),
+            colorImage: "gradient4"),
         Category(
             name: "Путешествия",
             icon: "airplane",
-            colorImage: UIImage(named: "gradient5") ?? .backgroundMain),
+            colorImage: "gradient5"),
         Category(
             name: "Развлечения",
             icon: "gamecontroller.fill",
-            colorImage: UIImage(named: "gradient6") ?? .backgroundMain),
+            colorImage: "gradient6"),
         Category(
             name: "Рестораны и кафе",
             icon: "fork.knife",
-            colorImage: UIImage(named: "gradient7") ?? .backgroundMain),
+            colorImage: "gradient7"),
         Category(
             name: "Транспорт",
             icon: "car.fill",
-            colorImage: UIImage(named: "gradient8") ?? .backgroundMain),
+            colorImage: "gradient8"),
         Category(
             name: "Фитнес",
             icon: "figure.cooldown",
-            colorImage: UIImage(named: "gradient9") ?? .backgroundMain),
+            colorImage: "gradient9"),
         Category(
             name: "Другое",
             icon: "ellipsis.circle.fill",
-            colorImage: UIImage(named: "gradient10") ?? .backgroundMain),
+            colorImage: "gradient10"),
     ]
     
     let currency: [Character] = ["₽", "€", "$"]
     
+    private init() {}
+}
+
+final class TransactionStore {
+    static let shared = TransactionStore()
+    
+    // Список всех транзакций
+    private var transactions: [Transaction] = []
+    
+    // Добавление транзакции
+    func addTransaction(_ transaction: Transaction) {
+        transactions.append(transaction)
+    }
+    
+    // Получение всех транзакций по категории
+    func transactions(for categoryName: String, from allTransactions: [Transaction]) -> [Transaction] {
+        allTransactions.filter { $0.category.name == categoryName }
+    }
+    
+    // Метод для получения суммы всех транзакций по каждой категории категории
+    func totalAmount(for category: Category, transactions: [Transaction]) -> Double {
+        return transactions.filter { $0.category.name == category.name }
+                           .reduce(0) { $0 + $1.amount }
+    }
+    
+    // Метод для получения баланса (сумма доходов минус сумма расходов)
+    func totalBalance() -> Double {
+        let incomeTotal = transactions.filter { $0.type == .income }.reduce(0) { $0 + $1.amount }
+        let expenseTotal = transactions.filter { $0.type == .expense }.reduce(0) { $0 + $1.amount }
+        return incomeTotal - expenseTotal
+    }
     private init() {}
 }
