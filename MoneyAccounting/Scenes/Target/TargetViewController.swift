b53929fbae7fb4dcad13ccb419a0535a41bde78d
Tatiana Lazarenko <tan.strebkova@gmail.com>
2024-03-09 17:56:09 +0300

    set logic of showing content on TargetVC

--- MoneyAccounting/Scenes/Target/TargetViewController.swift
+++ MoneyAccounting/Scenes/Target/TargetViewController.swift
@@ -14,13 +14,15 @@ final class TargetViewController: UIViewController {
     @IBOutlet var okayButton: UIButton!
     @IBOutlet var expenceLimitButton: UIButton!
     
+    @IBOutlet var mainLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         expenceLimitButton.setOrdinaryButton()
         okayButton.setAccentButton()
         
-        emotionImageView.image = UIImage(named: "sad")
+        setupUI()
     }
     
     @IBAction func doneAction() {
@@ -29,9 +31,28 @@ final class TargetViewController: UIViewController {
     
     private func setupUI() {
         let transactionStore = TransactionStore.shared
+        let goalsStore = GoalsStore.shared
         
-        let incomeTotal = transactions.filter { $0.type == .income }.reduce(0) { $0 + $1.amount }
-        let expenseTotal = transactions.filter { $0.type == .expense }.reduce(0) { $0 + $1.amount }
+        let transactions = transactionStore.getAllTransactions()
+        
+        let incomes = transactions.filter({ $0.type == .income })
+        let incomeTotal = incomes.reduce(0) { $0 + $1.amount }
+        
+        let expenses = transactions.filter { $0.type == .expense }
+        let expenseTotal = expenses.reduce(0) { $0 + $1.amount }
+        
+        let incomeGoal = goalsStore.goals.incomeGoal
+        let expenseLimit = goalsStore.goals.expenseLimit
+        
+        if expenseTotal > expenseLimit {
+            emotionImageView.image = UIImage(named: "sad")
+            mainLabel.text = "Слишком много расходов"
+        }
+        
+        if incomeTotal > incomeGoal {
+            emotionImageView.image = UIImage(named: "happy")
+            mainLabel.text = "Красавчик!"
+        }
         
     }
 }
